b7eed34c481518a0d20ac29b1ba01fe94f5f3cb0
alijawad1511 <jawad.bukhari1511@gmail.com>
2022-01-09 22:23:54 +0500

    User and Notes Model added

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/userRegistration')
+mongoose.connect('mongodb://localhost:27017/iNotebook')
 .then(() => {
     console.log('Connected to MongoDB Successfully...');
 }).catch((e) => {
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: Boolean,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+module.exports = mongoose.model('user', userSchema);
